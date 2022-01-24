3d1d6bafb62a16a119a57beb363179886e223d50
brittany melton <brittanymelton26@gmail.com>
2022-01-24 16:07:03 -0500

    static exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(18);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(18);
+
+            Console.WriteLine(celsius + "c");
+            Console.WriteLine(fahrenheit + "f");  
+
+        }
+    }
+
+
+}
