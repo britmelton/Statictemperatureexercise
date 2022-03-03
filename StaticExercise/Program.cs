002ce63e81ba276e9c9bfdb427d70d6b19188716
brittany melton <brittanymelton26@gmail.com>
2022-03-03 13:37:59 -0500

    added class exercise instruction commnets

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,6 +4,15 @@ namespace StaticExercise
 {
     internal class Program
     {
+        #region Exercise Instructions
+        /*Create a new console app called StaticExercise.
+         * Now create a static class called TempConverter.
+         * The class should have at least 2 methods:
+         * 1.one called FahrenheitToCelsius that will require a double as a parameter and return a double
+         * 2.the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+         * Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!*/
+
+        #endregion
         static void Main(string[] args)
         {
             var celsius = TempConverter.FahrenheitToCelsius(18);
@@ -11,9 +20,6 @@ namespace StaticExercise
 
             Console.WriteLine(celsius + "c");
             Console.WriteLine(fahrenheit + "f");  
-
         }
     }
-
-
 }
--- StaticExercise/TempConverter.cs
+++ StaticExercise/TempConverter.cs
@@ -8,19 +8,13 @@ namespace StaticExercise
 {
     public static class TempConverter
     {
-
-
         public static double FahrenheitToCelsius(double fahrenheit)
         {
-            return (fahrenheit - 32) * 5/9;
-      
+            return (fahrenheit - 32) * 5/9;     
         }
-
         public static double CelsiusToFahrenheit(double celsius)
         {
-            return (celsius * 9/5) + 32;
-            
-          
+            return (celsius * 9/5) + 32;                    
         }
     }
 }
